8d654c83a0e4f3a966c93aa52ebf0d8722fa75ee
Adam Vandenberg <flangy@gmail.com>
2011-03-22 08:53:39 -0700

    wip

--- Library/Formula/mapnik.rb
+++ Library/Formula/mapnik.rb
@@ -1,29 +1,35 @@
 require 'formula'
 
-class Mapnik <Formula
+class Mapnik < Formula
   url 'http://download.berlios.de/mapnik/mapnik-0.7.1.tar.gz'
   homepage 'http://www.mapnik.org/'
   md5 '3a070fdd7c6a3367ad78d95c2387b03b'
 
+  depends_on 'pkg-config' => :build
   depends_on 'scons' => :build
   depends_on 'libtiff'
   depends_on 'jpeg'
   depends_on 'proj'
   depends_on 'icu4c'
   depends_on 'boost'
+  depends_on 'cairomm' => :optional
 
   def install
-    tiff_prefix = Formula.factory("libtiff").prefix
-    jpeg_prefix = Formula.factory("jpeg").prefix
-    proj_prefix = Formula.factory("proj").prefix
-    icu_prefix = Formula.factory("icu4c").prefix
-    boost_prefix = Formula.factory("boost").prefix
+    ENV.x11 # for freetype-config
+
+    # Allow compilation against boost 1.46.0
+    inreplace ["src/datasource_cache.cpp", "src/libxml2_loader.cpp", "src/load_map.cpp", "src/tiff_reader.cpp"],
+      "#include <boost/filesystem/operations.hpp>",
+      "#define BOOST_FILESYSTEM_VERSION 2\n#include <boost/filesystem/operations.hpp>"
+
+    icu = Formula.factory("icu4c")
+
     system 'scons', "PREFIX=#{prefix}",
-        "TIFF_INCLUDES=#{tiff_prefix}/include",   "TIFF_LIBS=#{tiff_prefix}/lib",
-        "JPEG_INCLUDES=#{jpeg_prefix}/include",   "JPEG_LIBS=#{jpeg_prefix}/lib",
-        "PROJ_INCLUDES=#{proj_prefix}/include",   "PROJ_LIBS=#{proj_prefix}/lib",
-        "ICU_INCLUDES=#{icu_prefix}/include",     "ICU_LIBS=#{icu_prefix}/lib",
-        "BOOST_INCLUDES=#{boost_prefix}/include", "BOOST_LIBS=#{boost_prefix}/lib",
+        "ICU_INCLUDES=#{icu.include}", "ICU_LIBS=#{icu.lib}",
+        "TIFF_INCLUDES=#{HOMEBREW_PREFIX}/include", "TIFF_LIBS=#{HOMEBREW_PREFIX}/lib",
+        "JPEG_INCLUDES=#{HOMEBREW_PREFIX}/include", "JPEG_LIBS=#{HOMEBREW_PREFIX}/lib",
+        "PROJ_INCLUDES=#{HOMEBREW_PREFIX}/include", "PROJ_LIBS=#{HOMEBREW_PREFIX}/lib",
+        "BOOST_INCLUDES=#{HOMEBREW_PREFIX}/include", "BOOST_LIBS=#{HOMEBREW_PREFIX}/lib",
         "install"
   end
 end

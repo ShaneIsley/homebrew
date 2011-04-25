5c9092b2fa89a7ba4bc7f10e71becf14330212ff
Adam Vandenberg <flangy@gmail.com>
2011-04-25 13:56:42 -0700

    tig - update test code

--- Library/Formula/tig.rb
+++ Library/Formula/tig.rb
@@ -5,7 +5,8 @@ class Tig < Formula
   homepage 'http://jonas.nitro.dk/tig/'
   md5 'f373343199422c59518776db448dec0e'
 
-  depends_on 'spoo' => :python
+  # depends_on 'spoo' => :python
+  depends_on ExternalDependency.new(:python, 'spoo', 'spoo_module')
 
   def install
     system "./configure", "--prefix=#{prefix}"

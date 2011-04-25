44d390298a5f39d8a31e45643d70b4efc58b354f
Adam Vandenberg <flangy@gmail.com>
2011-04-25 13:32:37 -0700

    ExternalDependency class

--- Library/Homebrew/dependencies.rb
+++ Library/Homebrew/dependencies.rb
@@ -22,3 +22,24 @@ class Dependency
     @name = other_dep.to_s
   end
 end
+
+
+class ExternalDependency
+  attr_reader :type, :module_name, :install_name
+
+  def initialize type, module_name, install_name=nil
+    @type = type
+    @module_name = module_name
+    @install_name = install_name or module_name
+  end
+
+  def to_s
+    @install_name
+  end
+
+  def ==(other)
+    :type == other.type &&
+    :module_name == other.module_name &&
+    :install_name == other.install_name
+  end
+end
